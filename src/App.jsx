1e12ff013a0a15a1265a68822734d48f8e34da08
Elvis M <u201112959@upc.edu.pe>
2024-06-11 12:57:42 -0500

    iniciando el proyecto en blanco

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import "./App.css";
-import ContadorReducer from "./components/ContadorReducer";
-import ContadorFunciones from "./components/ContadorFunciones";
-import { MiBoton } from "./components/MiBoton";
 
 function App() {
-  return (
-    <>
-      <MiBoton texto="Mi Boton Creado" />
-      <ContadorReducer />
-      <ContadorFunciones />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
