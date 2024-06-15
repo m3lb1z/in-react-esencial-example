b013b22b3bd4bcceff8500647309f1bbe5584fe4
Elvis M <u201112959@upc.edu.pe>
2024-06-15 16:17:53 -0500

    utilizando la libreria styled-components para crear mini-componentes con estilos propios de diseño

--- src/components/Libro/Libro.jsx
+++ src/components/Libro/Libro.jsx
@@ -1,11 +1,20 @@
 import "./Libro.css";
 import PropTypes from "prop-types";
+import { styled } from "styled-components";
 
 const Libro = ({ libro }) => {
+  const Titulo = styled.h2`
+    display: block;
+    padding: 0;
+    color: blue;
+    margin: 0;
+    flex-basis: 100%;
+  `;
+
   return (
     <>
       <div className="item">
-        <h2>{libro.titulo}</h2>
+        <Titulo>{libro.titulo}</Titulo>
         <span>Publicado en {libro.publicado}</span>
       </div>
     </>
