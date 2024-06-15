3e98a78e0f28f2b6eaa3481bfdcbe002f3057b5a
Elvis M <u201112959@upc.edu.pe>
2024-06-15 10:20:21 -0500

    renderizando listas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Catalogo from "./components/Catalogo";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Catalogo />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Catalogo.jsx
@@ -0,0 +1,69 @@
+const Catalogo = () => {
+  const productos = [
+    {
+      id: 1,
+      nombre: "Camisa blanca",
+      precio: 15.99,
+    },
+    {
+      id: 2,
+      nombre: "Pantalones azules",
+      precio: 29.99,
+    },
+    {
+      id: 3,
+      nombre: "Sudadera gris",
+      precio: 24.99,
+    },
+    {
+      id: 4,
+      nombre: "Faldilla roja",
+      precio: 12.99,
+    },
+    {
+      id: 5,
+      nombre: "Camiseta negra",
+      precio: 8.99,
+    },
+    {
+      id: 6,
+      nombre: "Pantalones verdes",
+      precio: 39.99,
+    },
+    {
+      id: 7,
+      nombre: "Sudadera azul",
+      precio: 34.99,
+    },
+    {
+      id: 8,
+      nombre: "Faldón blanco",
+      precio: 19.99,
+    },
+    {
+      id: 9,
+      nombre: "Camisa verde",
+      precio: 14.99,
+    },
+    {
+      id: 10,
+      nombre: "Pantalones negros",
+      precio: 49.99,
+    },
+  ];
+
+  return (
+    <>
+      <h2>Catalogo de Productos</h2>
+      <ul>
+        {productos.map((producto) => (
+          <li key={producto.id}>
+            {producto.nombre} - S/ {producto.precio}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Catalogo;
