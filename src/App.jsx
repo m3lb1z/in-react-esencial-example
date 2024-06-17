938f21ab131087d1b693016430ef79c10a644786
Elvis M <u201112959@upc.edu.pe>
2024-06-16 20:19:22 -0500

    mejorar la navegacion con estilos de bootstrap y usar Nav-Link para mostrar la pagina actual activa

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import About from "./components/About";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <nav>
-          <Link to="/">Inicio</Link>
-          <Link to="/about">About</Link>
+        <nav className="navbar navbar-expand navbar-light bg-light">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link">
+                Inicio
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/about" className="nav-link">
+                About
+              </NavLink>
+            </li>
+          </ul>
         </nav>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </>
   );
