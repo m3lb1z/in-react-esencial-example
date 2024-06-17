c65371f5f7567e13e48e2ee92de053676b55566b
Elvis M <u201112959@upc.edu.pe>
2024-06-16 20:25:46 -0500

    usar el hook useNavigate para crear una redireccion a la pagina inicio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,21 @@
+import { Button } from "bootstrap";
 import Contents from "./Contents";
+import { useNavigate } from "react-router-dom";
 
 const About = () => {
+  const navegar = useNavigate();
+
+  const manejarNavegacion = () => {
+    navegar("/");
+  };
+
   return (
     <>
       <h1>About</h1>
       <Contents />
       <Contents />
+
+      <button onClick={manejarNavegacion}>Retornar al inicio</button>
     </>
   );
 };
